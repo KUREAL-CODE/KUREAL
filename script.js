eb5cf6e3e3eeda2641020e48fb463ae2836865eb
KUREAL-CODE <satriaadampratama0828@gmail.com>
2024-07-26 11:59:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+document.getElementById('toggleButton1').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri1');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton2').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri2');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton3').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri3');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton4').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri4');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton5').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri5');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton6').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri6');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton7').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri7');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton8').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri8');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+document.getElementById('toggleButton8').addEventListener('click', function() {
+    var content = document.getElementById('toggleMateri8');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
